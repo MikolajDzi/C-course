6ffa96eaeb02f3c72c99ff06c8f7358e59b57bdf
MikolajDzi <mikolaj.dziubinski@interia.pl>
2020-02-28 14:12:54 +0100

    Added decimal, hexadecimal and binary representation of the result

--- First C Labs/main.c
+++ First C Labs/main.c
@@ -3,51 +3,62 @@
 
 int main()
 {
-    unsigned int b1,b2;
+    unsigned int b1,b2,binar;
     int shift = 0;
     int c = 0;
     char oper;
-    printf("Please give binary numbers. ");
+    printf("Please give numbers in decimal system: ");
     scanf("%d",&b1);
     scanf("%d",&b2);
     fflush(stdin);
+    printf("Now please choose one of the following operators [| & ~ < >]: ");
     scanf("%c",&oper);
 
     switch(oper)
     {
         case '|':
         c = b1 | b2;
-        printf("Result of OR operation in decimal. %d", c);
-        printf(" Result of OR operation n hexadecimal. %x", c);
         break;
 
         case '&':
         c = b1 & b2;
-        printf("Result of AND operation in decimal. %d", c);
-        printf(" Result of AND operation in hexadecimal. %x", c);
         break;
 
         case '~':
         c = ~b1;
-        printf("Result of NOT operation in decimal. %d", c);
-        printf(" Result of NOT operation in hexadecimal. %x", c);
         break;
 
         case '<':
         printf("Please give the number of digits you want to shift the number.\n");
         scanf ("%d",&shift);
         c = b1<<shift;
-        printf("Result of SHIFT LEFT operation in decimal. %d", c);
-        printf(" Result of SHIFT LEFT operation in hexadecimal. %x", c);
         break;
 
         case '>':
         printf("Please give the number of digits you want to shift the number.\n");
         scanf ("%d",&shift);
         c = b1>>shift;
-        printf("Result of SHIFT RIGHT operation in decimal. %d", c);
-        printf(" Result of SHIFT RIGHT operation in hexadecimal. %x", c);
         break;
     }
+    printf("Here is the representation of the result in decimal system: %d",c);
+    printf("\n");
+    printf("Here is the representation of the result in hexadecimal system: %x",c);
+    printf("\n");
+    printf("Here is the representation of the result in binary system: \n");
+    for (int i = 31; i>= 0; i--)
+        {
+            binar = c >> i;
+
+            if(binar & 1)
+            {
+                printf("1");
+            }
+            else
+            {
+                printf("0");
+            }
+        }
+   printf("\n");
+
     return 0;
 }
